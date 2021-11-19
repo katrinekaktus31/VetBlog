0bcad917bb3127fb9904aa605ebeb17ac9947fad
Kateryna Brudko <katyabru31@gmail.com>
2021-11-20 01:35:55 +0200

    The css was update Header was create with such image settings.py was update (correct send_mail param)

--- VetBlog/settings.py
+++ VetBlog/settings.py
@@ -145,5 +145,5 @@ SERVER_EMAIL = 'vetReproductionNUBip@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vetReproductionNUBip@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD'),
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
